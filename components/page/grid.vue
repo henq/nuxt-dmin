ea96a8513fff172bc8a5b53b1cc5b6d69e890019
zhouyx <taibaizhou@163.com>
2019-09-30 18:01:44 +0800

    ok

---
+++ components/page/grid.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <div class="search"></div>
+        <Table :columns="columns" :data="data"></Table>
+        <div class="pager"></div>
+    </div>
+</template>
+<script>
+export default {
+    name:"Grid",
+    props:{
+        page:{
+            type:Boolean,
+            default:true
+        },
+        search:{
+            type:object
+        },
+        columns:{
+            type: Array,
+            default:[]
+        },
+        url:{
+            type:String
+        },
+        data:{
+            type:Array
+        }
+    },
+    data(){
+        return {
+            data:[]
+        }
+    }
+}
+</script>
